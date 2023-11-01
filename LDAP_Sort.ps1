34459121efed5b3084353653e126da58bc37a53b
Risto Libera <qofgxnm7@gmail.com>
2023-11-01 15:47:35 +0900

    Project Complete

--- LDAP_Sort.ps1
+++ LDAP_Sort.ps1
@@ -1,3 +1,4 @@
+
 param (
     [Parameter(Mandatory = $true)]
     [string]
@@ -9,3 +10,105 @@ param (
     [string]
     $order = $args[2]
 )
+
+Add-Type -AssemblyName PresentationFramework
+$curDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$tempCSVFile = $curDir + "\" + "temp.csv"
+$tempSortedCSVFile = $curDir + "\" + "temp_sort.csv"
+$dnRegex = "^dn:.*"
+$linebreak = "`r`n"
+
+# Delete temp and old files
+try {
+    $ErrorActionPreference = "Stop"
+    if (Test-Path $tempCSVFile) {
+        Remove-Item -Path $tempCSVFile -Force
+    }
+    if (Test-Path $tempSortedCSVFile) {
+        Remove-Item -Path $tempSortedCSVFile -Force
+    }
+    if (Test-Path $resultLDAPFile) {
+        Remove-Item -Path $resultLDAPFile -Force
+    }
+} catch {
+    [System.Windows.MessageBox]::Show("Deletion Error!")
+    exit 1
+} finally {
+    $ErrorActionPreference = "Continue"
+}
+
+# Exit if ldif file is empty
+if ((Get-Item $srcLDAPFile).Length -eq 0) {
+    [System.Windows.MessageBox]::Show("Source file is empty!")
+    exit 0
+}
+
+# Convert ldif file to csv file
+try {
+    $ErrorActionPreference = "Stop"
+    Get-Content $srcLDAPFile | ForEach-Object {
+        if ($_ -eq "") {
+            $line = $line.Substring(0, $line.Length - 1)
+            Out-File -FilePath $tempCSVFile -InputObject $line -Encoding default -Append
+            $line = ""
+        } else {
+            $line = $line + '"' + $_ + '"' + ","
+        }
+    }
+
+    # Output final row
+    if ($line -ne "") {
+        $line = $line.Substring(0, $line.Length - 1)
+        Out-File -FilePath $tempCSVFile -InputObject $line -Encoding default -Append
+        $line = ""
+    }
+} catch {
+    [System.Windows.MessageBox]::Show("Conversion to csv Error!")
+    exit 1
+} finally {
+    $ErrorActionPreference = "Continue"
+}
+
+# Sort csv file
+try{
+    $ErrorActionPreference = "Stop"
+
+    if ($order -eq "0") {
+        Get-Content $tempCSVFile | Sort-Object { [string]$_.Split(",")[0].Substring(9)} | Out-File -FilePath $tempSortedCSVFile -Encoding default
+    } else {
+        Get-Content $tempCSVFile | Sort-Object { [string]$_.Split(",")[0].Substring(9)} -Descending | Out-File -FilePath $tempSortedCSVFile -Encoding default
+    }
+} catch {
+    [System.Windows.MessageBox]::Show("Sorting Error!")
+    exit 1
+} finally {
+    $ErrorActionPreference = "Continue"
+}
+
+# Convert csv file back to ldif file
+try{
+    $ErrorActionPreference = "Stop"
+    $isFirstRow = $true
+    Get-Content $tempSortedCSVFile | ForEach-Object {
+        $entryInfo = $_.Split('"').where({ ($_ -ne "") -and ($_ -ne ",") })
+        foreach ($info in $entryInfo) {
+            if (($info -match $dnRegex) -and ($isFirstRow -eq $true)) {
+                $line = $info
+                $isFirstRow = $false
+            } elseif ($info -match $dnRegex) {
+                $line = $linebreak + $info
+            } else {
+                $line = $info
+            }
+            Out-File -FilePath $resultLDAPFile -InputObject $line -Encoding default -Append
+        }
+    }
+} catch {
+    [System.Windows.MessageBox]::Show("Conversion to ldif Error!")
+    exit 1
+} finally {
+    $ErrorActionPreference = "Continue"
+}
+
+[System.Windows.MessageBox]::Show("Sort Complete!")
+exit 0
--- README.md
+++ README.md
@@ -1,22 +1,23 @@
 # Project: LDAP_Sort
 ## What does this achieve
 
-- Sort LDAP file by uid in ascending or descending order
+- Sort LDAP file by uid in ascending or descending order(ASCII)
 - Does not encounter memory leak error if file is too big
 
 ## Getting start
 
-- Put powershell(.ps1) script and original ldif file into the same folder
-- In command prompt run command below to generate a sorted ldif file
+- Put Powershell(.ps1) script and original ldif file into the same folder
+- Open command prompt or Powershell prompt on the same folder
+- In command prompt run command below to generate an ascending sorted ldif file
 ```cmd
-$ powershell.exe LDAP_Sort.ps1 -1 "sss" -2 "sss" -3 "sss"
+$ Powershell.exe LDAP_Sort.ps1 -1 "sss" -2 "sss" -3 "sss"
 ```
-
-- In powershell prompt run command below to generate a sorted ldif file
-```powershell
-$ LDAP_Sort.ps1 -1 "sss" -2 "sss" -3 "sss"
+- In command prompt run command below to generate a descending sorted ldif file
+```cmd
+$ Powershell.exe LDAP_Sort.ps1 -1 "sss" -2 "sss" -3 "sss"
 ```
 
 ## Notes
 
-- Can only sort when uid is contained in dn attribute
+- Can only sort when uid is contained in dn attribute like this
+$ dn: uid=jsmith1,ou=People,dc=example,dc=com
